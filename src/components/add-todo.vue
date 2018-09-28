cc9db2533cd648f7953e8696648d3bc0f2d7bb4c
LeanKhan <34924520+LeanKhan@users.noreply.github.com>
2018-09-28 01:46:49 +0100

    Edit AddTodo View
    Add Bootstrap.css via file upload

--- src/components/add-todo.vue
+++ src/components/add-todo.vue
@@ -25,6 +25,7 @@
 
     <!-- TodoList Content -->
     <div class="content">
+
       <ul>
       
         <li v-for="(todo, i) in todoList" v-bind:key="i" >
@@ -38,10 +39,14 @@
         </li>
 
       </ul>
+
     </div>
 
     <!-- TodoList footer -->
     <footer>
+      <p>double click to edit, press enter to save</p>
+      <p>by emmanuel-segun-lean</p>
+      <p><a href="https://twitter.com/leankhan_" target="_blank">twitter</a>  <a href="https://github.com/LeanKhan" target="_blank">github</a></p>
     </footer>
 
   </main>
@@ -55,33 +60,34 @@ export default {
     return {
       todo: "",
       todoList: [],
-      today: new Date(),
+      today: new Date()
     };
   },
   methods: {
     addTodo: function(todoItem) {
-      if(todoItem === ""){alert("Can't do that bro!")
-      }else{
-       this.todoList.push({
-        todoText: todoItem,
-        completed: false,
+      if (todoItem === "") {
+        alert("Can't do that bro!");
+      } else {
+        this.todoList.push({
+          todoText: todoItem,
+          completed: false
         });
-      this.todo = "";
+        this.todo = "";
       }
     },
-    focus: function(e){
+    focus: function(e) {
       // e.target.readonly = !e.target.readonly
       e.target.readOnly = !e.target.readOnly;
     },
-    editTodo: function(newText,id,e){
+    editTodo: function(newText, id, e) {
       this.todoList[id].todoText = newText;
       this.focus(e);
     },
-    deleteTodo: function(id){
-      this.todoList.splice(id,1);
+    deleteTodo: function(id) {
+      this.todoList.splice(id, 1);
     },
-    toggleCompleted: function(id){
-      this.todoList[id].completed = !this.todoList[id].completed
+    toggleCompleted: function(id) {
+      this.todoList[id].completed = !this.todoList[id].completed;
     }
   }
 };
@@ -91,27 +97,46 @@ export default {
 .todo-input {
   font-size: 24px;
 }
-  ul {
-    list-style-type: none;
-  }
-  a {
-   padding: 5px;
-    cursor: pointer !important;
-  }
-  a:hover {
-    text-decoration: underline;
-  }
-  .completed {
-    text-decoration: line-through !important;
-    color: gray;
-  }
-  aside {
-    margin: 20px;
-  }
-  input[type="checkbox"] {
-    width: 25px;
-    height: 25px;
-    outline: none;
-  }
+ul {
+  list-style-type: none;
+}
+a {
+  padding: 5px;
+  cursor: pointer !important;
+}
+footer p a {
+  color: #4d4d4d !important;
+  text-decoration: none !important;
+}
+footer p a:hover {
+  color: #61df97!important;
+  text-decoration: dotted !important;
+}
+footer p {
+  font-size: 12px;
+  color: #4d4d4d !important;
+}
+a:hover {
+  text-decoration: underline;
+}
+.completed {
+  transition: all .2s;
+  text-decoration: line-through !important;
+  color: gray;
+}
+aside {
+  margin: 20px;
+}
+input[type="checkbox"] {
+  width: 25px;
+  height: 25px;
+  outline: none;
+}
+footer {
+  margin-top: 40px;
+}
+main {
+  padding-left: 10rem;
+  padding-right: 10rem;
+}
 </style>
-
